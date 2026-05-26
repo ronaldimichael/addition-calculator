d066d332869edb3df5c24bb30fceea84a4b38dd5
Ronaldi <ronaldimikel@gmail.com>
2026-05-26 17:37:43 +0700

    Add result container to HTML and implement basic JavaScript functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+const operandA = document.querySelector("#container-addition_firstNumber");
+
+const operandB = document.querySelector("#container-addition_secondNumber");
+
+const calculateBtn = document.querySelector("button");
+
+const result = document.querySelector(".container-result");
