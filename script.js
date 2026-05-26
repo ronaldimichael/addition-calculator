eb461c743cd32224dc7f6264d54afaac6617aff1
Ronaldi <ronaldimikel@gmail.com>
2026-05-27 03:17:38 +0700

    Refactor HTML structure and enhance JavaScript functionality for addition calculation

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const operandB = document.querySelector("#container-addition_secondNumber");
 const calculateBtn = document.querySelector("button");
 
 const result = document.querySelector(".container-result");
+
+function sum() {
+  const add =
+    Number(parseInt(operandA.value)) + Number(parseInt(operandB.value));
+  return add;
+}
+
+calculateBtn.addEventListener("click", () => {
+  const firstNum = Number(parseInt(operandA.value));
+  const secondNum = Number(parseInt(operandB.value));
+  result.textContent = `The result of ${firstNum} and ${secondNum} is ${sum()}`;
+});
